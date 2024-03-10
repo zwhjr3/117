be49ad34197279cc46e2eab347495c50d1f5c2ac
zwhjr3 <82552046+zwhjr3@users.noreply.github.com>
2024-03-09 21:35:43 -0600

    Add files via upload

---
+++ 117.js
@@ -0,0 +1,16 @@
+array_1=[1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+
+random_no = Math.floor((Math.random()*array_1.length)+1);
+
+
+Element_of_array = array_1[random_no];
+
+console.log(random_no);
+
+sketch = random_no;
+
+timer_counter = 0;
+timer_check = "";
+drawn_sketch = "";
+answer_holder = "";
+score = 0;
